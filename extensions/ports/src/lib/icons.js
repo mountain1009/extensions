ea4552227dfeb85242877df06873b0934188c6e0
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2026-07-01 21:45:31 +0200

    fixes

--- extensions/ports/src/lib/icons.js
+++ extensions/ports/src/lib/icons.js
@@ -13,7 +13,7 @@ const ICONS = {
     '<path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/><path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"/><path d="M10 11v6"/><path d="M14 11v6"/>',
 };
 
-export default function icon(name, size = 14, className = "", strokeWidth = 2) {
+export default function icon(name, size = 14, className = "", strokeWidth = 1.5) {
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttribute("viewBox", "0 0 24 24");
   svg.setAttribute("width", String(size));
